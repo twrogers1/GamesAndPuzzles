7a43c5416efa9093549eabefafddbf7c80464bc4
Taylor Rogers <twardr@gmail.com>
2020-12-15 10:08:40 -0500

    Bug fixes and improvements
    - updated __str__() to make the output much more readable
    - made a couple more variables "private"
    - fixed bug with how coords between two points were generated, which was causing some datasets to not properly highlight in the __str__
    - renamed _make_board() to _make_grid for consistency and fixed a bug which would not ignore spaces between letters on the input

--- word_search.py
+++ word_search.py
@@ -1,45 +1,43 @@
 class Puzzle():
     def __init__(self, grid: str, word_bank: list):
-        self.grid = self._make_board(grid)
+        self.grid = self._make_grid(grid)
         self.bank = word_bank
-        self.counter = 0 # count of checked words
-        self.found_coords = [] # (y,x) coordinates for cells contained in found words
+        self._counter = 0 # count of checked words
+        self._found_coords = [] # (y,x) coordinates for cells contained in found words
     
 
     def __str__(self):
         grid = self.grid.copy()
         
-        # set font weight for each character
-        for y, row in enumerate(grid):
-            for x, char in enumerate(row):
-                if (y,x) in self.found_coords:
-                    char = "\033[1m" + "\033[3m" + char.upper() + "\033[0m" # bold, italics, upper
-                else:
-                    char = "\033[2m" + char.lower() + "\033[0m" # light, lower
-                grid[y][x] = char
+        if len(self._found_coords) > 0:
+            # set font weight for each character
+            for y, row in enumerate(grid):
+                for x, char in enumerate(row):
+                    if (y,x) in self._found_coords:
+                        char = "\033[1m" + "\033[3m" + char.upper() + "\033[0m" # bold, italics, upper
+                    else:
+                        char = "\033[2m" + char.lower() + "\033[0m" # light, lower
+                    grid[y][x] = char
                 
-        string = ""
+        string = "\n-=  W o r d   S e a r c h  =-\n\n"
         for row in grid:
-            border = "-" * (len(row)*4) + "-"
-            string += border
             string += "\n"
-            string += "| " + " | ".join(row) + " |"
+            string += "   ".join(row)
             string += "\n"
-        string += border
         string += "\n\nWord Bank:\n"
         string += "- " + "\n- ".join(self.bank)
         string += "\n"
         return string
 
 
-    def _make_board(self, grid_str):
+    def _make_grid(self, grid_str):
         """ Take in the :grid_str: to create a 2-d list of lists """
         grid = []
         for row in grid_str.splitlines():
             row = row.strip().upper()
             if row == "":
                 continue
-            grid.append(list(row))
+            grid.append([r.strip() for r in list(row) if r.strip() != ""])
 
         return grid
 
@@ -49,18 +47,17 @@ class Puzzle():
         y1,x1 = c1
         y2,x2 = c2
         
-        y_diff = 0 if y1-y2 == 0 else 1
-        x_diff = 0 if x1-x2 == 0 else 1
-        
+        y_diff = 0 if y1==y2 else -1 if y1>y2 else 1  
+        x_diff = 0 if x1==x2 else -1 if x1>x2 else 1    
+    
         coords = []
-        while True:
-            if (y1,x1) > (y2,x2):
-                break
+        iters = max([abs(y1-y2), abs(x1-x2)])+1
+        for _ in range(iters):
             coords.append((y1,x1))
             x1 += x_diff
             y1 += y_diff
-
-        self.found_coords.extend(coords)
+        
+        self._found_coords.extend(coords)
 
 
     def _look_coord(self, y,x):
@@ -88,7 +85,7 @@ class Puzzle():
         """ Check if the given word is in the word bank. If yes, return the word, else return False """
         bank = [b.upper() for b in self.bank]
         word = word.upper()
-        self.counter += 1
+        self._counter += 1
         if word in bank:
             return word
         elif self._reverse(word) in bank:
@@ -141,26 +138,30 @@ class Puzzle():
                     found.extend(self._scan(y,x, deltay,deltax))
     
         found_string_parts = []
-        found_string_parts.append(f"Found {len(found)} words in {self.counter*2:,} searches:")
+        found_string_parts.append(f"Found {len(found)} words in {self._counter*2:,} searches:")
         for f in found:
             word, start, end = f
             found_string_parts.append(f"- {word} found between ({start[0]+1},{start[1]+1}) and ({end[0]+1},{end[1]+1}).")
-        found_string_parts.append("* Coordinates start in the top-left corner! The first letter is (x=1,y=1)")
+        found_string_parts.append("\n* Coordinates start in the top-left corner! The first letter is (x=1,y=1)")
         found_string_parts.append("* As x increases, move right. As y increases, move down.")
+        found_string_parts.append("\n\n")
         
         found_string = "\n".join(found_string_parts)
+        
+        print(self)
+        print(found_string)
         return found_string
 
 
 if __name__ == "__main__":
     grid = """
-    JSOLUTIS
-    SUNARUUA
-    NEPTUNET
-    SONIEISU
-    RCEVTRER
-    AHTRAESN
-    MMERCURY
+    J S O L U T I S
+    S U N A R U U A
+    N E P T U N E T
+    S O N I E I S U
+    R C E V T R E R
+    A H T R A E S N
+    M M E R C U R Y
     """
     word_bank = [w.strip() for w in """
     Earth
@@ -172,9 +173,6 @@ if __name__ == "__main__":
     Uranus
     Venus
     """.split("\n") if w.strip() != ""]
-    
+
     p = Puzzle(grid, word_bank)
-    
-    found = p.solve()
-    print(p)
-    print(found)
+    p.solve()
