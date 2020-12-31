2f11179fd64ac2867359add2bb60ed277de31dc7
Taylor Rogers <twardr@gmail.com>
2020-12-30 20:59:40 -0500

    name game revised
    - added more names, enough for one of each letter A-Z
    - sorted names
    - added a simple readme
    - added rules in the script
    - changed input logic so entering a name a second time doesn't automatically lose the game, you can keep trying

--- NameGame/namegame.py
+++ NameGame/namegame.py
@@ -1,16 +1,33 @@
 import random
 import re
+import time
 from collections import defaultdict
 
 
-# debugging
-import os
-os.chdir("/Users/taylor.rogers/Desktop/Python/GitHub/GamesAndPuzzles/NameGame")
-
-
 class NameGame():
+    __RULES__ = """
+    -= N A M E   G A M E =-
+    
+    Rules:
+    - Players take turns naming people, typically celebrities
+    - The named person must have a first and last name (In other words, Seal isn't a valid choice)
+    - The choice on turn 1 can be any person. In this script, it's chosen by the CPU randomly
+    - For the choices on turn 2 and after, the first letter of the first name must start with the first letter of the last name from the prior choice.
+        (For example, if James Bond was chosen on turn 1, Bruce Wayne would be a valid choice on turn 2, Walter White would be a valid choice on turn 3, and so on)
+    - Names cannot be repeated
+    - The game ends when players run out of choices
+
+    Pro Tips:
+    - Try to remember names (lelz)
+    - The scope of the people bank can be expanded however you like
+        - Try using fictional characters, authors, or people you know IRL
+
+    Good luck!
+
+    (During your turn, type 'q' or 'quit' to quit.)
+    """
     def __init__(self):
-        self.name_bank = self._parse_names() # word bank for the CPU layer - stored in lowercase
+        self.name_bank = self._parse_names() # word bank for the CPU player - stored in lowercase
         self.played = [] # names already played - stored in lowercase
 
 
@@ -49,24 +66,24 @@ class NameGame():
         self.name_bank[letter].remove(name.lower())
 
 
-    def _choose_random_name(self, letter):
-        """ Internal method - Choose a random name from the self.name_bank for the given :letter: """        
+    def _choose_random_name(self, letter, remove=True):
+        """ Internal method - Choose a random name from the self.name_bank (and by defualt, :remove: it) for the given :letter: """        
         letter = letter.lower()
         names = list(self.name_bank[letter])
         if len(names) > 0:
             choice = random.choice(names)
-            self._remove_from_bank(choice)
+            if remove:
+                self._remove_from_bank(choice)
         else:
-            # print(f"!! No choices left for {letter.upper()}")
             choice = None       
     
         return choice
 
 
     def _trim_postfix(self, name):
-        """ Given a fullname :name:, parse out the common postfixes (Sr., Jr., III, etc.) Returns a name string. """
+        """ Given a fullname :name:, parse out the common suffixes (Sr., Jr., III, etc.). Returns a name string. """
         
-        def _is_postfix(part):
+        def _is_suffix(part):
             """ Determine if the given name :part: is a postfix. Returns a bool. """
             # sub out all periods - as seen in "Sr."
             # compare against Sr., Jr., and roman numerals 1-10
@@ -79,26 +96,30 @@ class NameGame():
             else:
                 return False
 
-        parts = [p for p in name.split(" ") if not _is_postfix(p)]
+        parts = [p for p in name.split(" ") if not _is_suffix(p)]
         return " ".join(parts)
 
 
     def parse_last_name(self, name):
-        """ Given a fullname :name:, parse it up so the first letter of the last name is returned. """
+        """ Given a fullname :name:, parse it so the first letter of the last name is returned. """
         name = self._trim_postfix(name)
         return name.split(" ")[-1][0]
 
 
     def get_name_input(self, letter=None):
-        """ Get input from the user and validate it. """
+        """ Get input from the user and validate it before returning. """
         name = None
         try:
             while not name:
                 prompt = f"Enter a name starting with {letter.upper()} > " if letter else "Enter a name > "
                 name = input(prompt).strip()
                 name = re.sub(r"\s{2,}", " ", name) # handle multiple spaces in the input
+
+                # quit
+                if name.lower() in "q, quit, stop, exit".split(", "):
+                    return None
                 
-                # name should contain 2+, non-postfix words
+                # name should contain 2+, non-suffix words
                 if " " not in self._trim_postfix(name):
                     print("!! Name must contain two or more words.")
                     name = None
@@ -109,9 +130,14 @@ class NameGame():
                     print(f"!! Name must start with the letter {letter.upper()}.")
                     name = None
                     continue
+
+                # name can't have been played previously
+                if name in self.played:
+                    print("!! Name was already plaed. Try another.")
+                    name = None
+                    continue
                 
-                if name.lower() in "q, quit, stop, exit".split(", "):
-                    return None
+
         except KeyboardInterrupt:
             return None
         
@@ -129,10 +155,11 @@ class NameGame():
             while True:
                 name = self._choose_random_name(letter)    
                 if name is None:
-                    break
+                    break # computer ran out of options.
                 self.played.append(name.lower())
                 letter = self.parse_last_name(name)
         elif players == 1:
+            print(NameGame.__RULES__)
             your_turn = False
             while True:
                 name = self.get_name_input(letter=letter) if your_turn else self._choose_random_name(letter)
@@ -141,18 +168,19 @@ class NameGame():
                         print("You win, the computer ran out of options.")
                     else:
                         print("\nGame over, you quit the game.")
-                    break
-                elif name.lower() in self.played:
-                    print(f"You lose, name '{name}' was already played!")
-                    break               
+                    break           
                 else:
+                    if not your_turn:
+                        time.sleep(1) # create the illusion of the CPU thinking
                     print(f"{'You' if your_turn else 'CPU'} chose {name.title()}.")
                     self.played.append(name.lower())
                     letter = self.parse_last_name(name)
                     your_turn = not your_turn # flip back and forth between yours and the computer's turn    
+        
         print(self)
         
             
 if __name__ == "__main__":
     game = NameGame()
     game.play(players=1)
+    # game.play(players=0)
---
+++ NameGame/readme.md
@@ -0,0 +1,9 @@
+# Name Game
+
+Run the `namegame.py` file to see the rules and play. Ensure the terminal's pwd is the same as the .py file so the `names.txt` file can be read by the script.
+
+## TODO
+- add more names for the CPU player
+- add support for 2 local players
+- adding coloring to the console output
+- add support for a timed mode to make it sweaty
